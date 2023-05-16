522427490a6cdc112c46e425988c43d2c88439ba
wangyi <wanyi19960320@163.com>
2023-05-16 15:07:48 +0800

    项目起步

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
+// 创建路由实例
 const router = createRouter({
+  // 使用hash方式实现路由
   history: createWebHashHistory(),
+  // 配置路由规则，写法和之前一样
   routes
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import { createStore } from 'vuex'
+import caratePersistedstate from 'vuex-persistedstate'
 
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
+
+// 创建vuex仓库并导出
 export default createStore({
   state: {
-  },
-  getters: {
+    // 数据
   },
   mutations: {
+    // 改数据函数
   },
   actions: {
+    // 请求数据函数
   },
   modules: {
-  }
+    // 分模块
+    cart,
+    user,
+    category
+  },
+  getters: {
+    // vuex的计算属性
+  },
+  plugins: [
+    // vuex持久化插件
+    caratePersistedstate({
+      // 持久化的数据
+      key: 'erabbit-client-pc',
+      // 持久化的模块
+      paths: ['user', 'cart']
+    })
+  ]
+
 })
